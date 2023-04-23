060769d596ee848f91e37f2456c4c82fa360f84f
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-04-23 20:09:02 +0200

    login
    login failed due to path issues i think.

---
+++ src/js/api/auth/login.mjs
@@ -0,0 +1,18 @@
+/*import { HOST_API_URL } from "../constants.mjs";
+import { LOGIN_API_URL } from "../constants.mjs";
+
+export async function login(profile) {
+  const loginURL = HOST_API_URL + LOGIN_API_URL;
+  const body = JSON.stringify(profile);
+
+  const response = await fetch(loginURL, {
+    headers: {
+      "Content-type": "application/json",
+    },
+    method: "POST",
+    body,
+  });
+
+  const result = await response.json();
+  console.log(result);
+}*/
--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,4 +1,13 @@
 //Functions
 import { setRegisterFormListener } from "./handlers/register.mjs";
+//import { setLoginFormListener } from "./handlers/login.mjs";
 
 setRegisterFormListener();
+
+//const path = location.pathname;
+/*
+if (path === "/profile/login/index.html") {
+  setLoginFormListener();
+} else if (path === "/profile/register/index.html") {
+  setRegisterFormListener();
+}*/

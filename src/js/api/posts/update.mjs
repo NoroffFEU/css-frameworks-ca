45d54938b9605297359dfc90fc1217d7ba1fff4e
EM-90 <94170154+EM-90@users.noreply.github.com>
2023-04-30 16:46:40 +0200

    create and update

--- src/js/api/posts/update.mjs
+++ src/js/api/posts/update.mjs
@@ -1,1 +1,14 @@
 export async function updatePost(id) {}
+
+import { HOST_API_URL, POSTS } from "../constants.mjs";
+import { authFetch } from "../authFetch.mjs";
+
+export async function updatePost(postData) {
+  const createPostURL = `${HOST_API_URL}${POSTS}/${postData.id}`;
+
+  const response = await authFetch(createPostURL, {
+    method: "Put",
+    body: JSON.stringify(postData),
+  });
+  return await response.json();
+}
--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,6 +1,7 @@
 //Functions
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setloginFormListener } from "./handlers/login.mjs";
+import { createPost } from "./api/posts/create.mjs";
 
 const path = location.pathname;
 
@@ -9,3 +10,8 @@ if (path === "/profil/login/index.html") {
 } else if (path === "/profil/register/index.html") {
   setRegisterFormListener();
 }
+
+createPost({
+  title: "Example",
+  body: "hello there",
+});

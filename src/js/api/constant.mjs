bd64e4ec6b8aeee65e1e514f5bfd4aed0ddfc992
Mare94 <mn-sand@online.no>
2023-05-08 17:18:40 +0200

    exported base api url

--- src/js/api/constant.mjs
+++ src/js/api/constant.mjs
@@ -1,1 +1,1 @@
-const BASE_API_URL = `https://nf-api.onrender.com/api/v1/social`;
+export const BASE_API_URL = `https://nf-api.onrender.com/api/v1/social`;
---
+++ src/js/index.mjs
@@ -0,0 +1,3 @@
+import * as constant from "./api/constant.mjs";
+
+console.log(constant.BASE_API_URL);

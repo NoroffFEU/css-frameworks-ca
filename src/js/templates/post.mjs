3f1251db27bd069f0dffe17834f18b958e756991
Mare94 <mn-sand@online.no>
2023-05-10 17:33:46 +0200

    added body innertext

--- src/js/templates/post.mjs
+++ src/js/templates/post.mjs
@@ -2,6 +2,7 @@ export function postTemplate(postData) {
     const post = document.createElement("div");
     post.classList.add("post");
     post.innerText = postData.title;
+    post.innerText = postData.body;
     return post;
 }
 

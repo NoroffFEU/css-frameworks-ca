7742dc0f1d4e2e3ab224dd9b80210cdb4c24d917
Mare94 <mn-sand@online.no>
2023-05-10 16:23:38 +0200

    created authToken, createpost

--- src/js/api/posts/create.mjs
+++ src/js/api/posts/create.mjs
@@ -1,1 +1,18 @@
-export async function createPost(postData) {}
+
+import { authToken } from "../authToken.mjs";
+import { BASE_API_URL } from "../constant.mjs";
+
+const action = "/posts";
+const method = "post"
+
+export async function createPost(postData) {
+    const createPostUrl = BASE_API_URL + action;
+    
+    const response = await authToken(createPostUrl, {
+        method,
+        body: JSON.stringify(postData)
+
+    });
+
+    return await response.JSON();
+}

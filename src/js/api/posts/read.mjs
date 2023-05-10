59d5e04ff8ead1044ed7dd714612138939092a50
Mare94 <mn-sand@online.no>
2023-05-10 16:45:22 +0200

    added remove, read and update

--- src/js/api/posts/read.mjs
+++ src/js/api/posts/read.mjs
@@ -1,3 +1,35 @@
-export async function readPosts() {}
 
 export async function readPost(id) {}
+
+
+import { authToken } from "../authToken.mjs";
+import { BASE_API_URL } from "../constant.mjs";
+
+const action = "/posts";
+
+
+export async function readPosts() {
+
+
+    const readPostsUrl = `${BASE_API_URL}${action}`;
+    
+    const response = await authToken(readPostsUrl);
+
+    return await response.JSON();
+}
+
+
+
+export async function readPost(id) {
+
+    if (!id) {
+        throw new Error("Read post requieres an ID")
+    }
+
+
+    const readPostUrl = `${BASE_API_URL}${action}/${id}`;
+    
+    const response = await authToken(readPostUrl);
+
+    return await response.JSON();
+}
--- src/js/api/posts/update.mjs
+++ src/js/api/posts/update.mjs
@@ -1,1 +1,17 @@
-export async function updatePost(id) {}
+import { authToken } from "../authToken.mjs";
+import { BASE_API_URL } from "../constant.mjs";
+
+const action = "/posts";
+const method = "put"
+
+export async function updatePost(postData) {
+    const updatePostUrl = `${BASE_API_URL}${action}/${postData.id}`;
+    
+    const response = await authToken(updatePostUrl, {
+        method,
+        body: JSON.stringify(postData)
+
+    });
+
+    return await response.JSON();
+}

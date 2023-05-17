336e38f3b7db13702f5bc61899c44185714fdf95
saibia8 <saibia.design@gmail.com>
2023-05-17 11:49:59 +0200

    Register, login and storage

---
+++ src/js/api/auth/login.mjs
@@ -0,0 +1,23 @@
+import {API_SOCIAL_URL} from "../constants.mjs";
+import * as storage from "../../storage/index.mjs";
+
+const action = "/auth/login";
+const method = "post";
+
+export async function login(profile) {
+const loginUrl = API_SOCIAL_URL + action;
+const body = JSON.stringify(profile);
+
+const response = await fetch(loginUrl, {
+   headers: {
+      "Content-Type": "application/json"
+   },
+   method,
+   body
+});
+const {accessToken, ...user} = await response.json();
+storage.save("token", accessToken);
+storage.save("profile", user);
+alert (`${user.name} tu pasijunges`);
+};
+
---
+++ src/js/index.mjs
@@ -0,0 +1,14 @@
+import { setRegisterFormListener } from "./handlers/register.mjs";
+import { setLoginFormListener } from "./handlers/login.mjs";
+
+setRegisterFormListener();
+setLoginFormListener();
+
+
+
+
+
+//import * as constants from "./api/constants.mjs";
+//import * as register from "./handlers/register.mjs";
+
+//console.log(constants.API_SOCIAL_URL);

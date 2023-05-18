99af4e1f73a77c5baad9c34d2ba7488ef5e7d080
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-05-18 10:46:42 +0200

    Debugging getSinglePost

--- src/js/handlers/posts/createPost.js
+++ src/js/handlers/posts/createPost.js
@@ -3,15 +3,6 @@ import { createComment } from "../../api/posts/index.js";
 
 export function setCreatePostFormListener() {
     const form = document.getElementById("createPostForm");
-// alert("form funciona")
-    // COMO COLOCAR A FOTO DO PERFIL
-    // const userAvatar = document.querySelector(".user-avatar");
-    // userAvatar.innerHTML = `    ${
-    //     profile.author.avatar
-    //       ? `<img src="${postData.author.avatar}" class="rounded-circle" height="50" onerror="this.src='/images/user-icon-image-placeholder.jpg'" alt="${postData.author.name}">`
-    //       : `<img src="/images/user-icon-image-placeholder.jpg" class="rounded-circle" height="50" alt="${postData.author.name}">`
-    //   }`
-
     if (form) {
         form.addEventListener("submit", (event) => {
             event.preventDefault();

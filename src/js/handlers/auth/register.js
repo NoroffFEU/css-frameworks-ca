98a674cfc98131a0f4b55a100014da44338cd022
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-05-18 17:07:50 +0200

    Debug register.

--- src/js/handlers/auth/register.js
+++ src/js/handlers/auth/register.js
@@ -15,6 +15,11 @@ export function setRegisterFormListener() {
 
             //Send to the API
             register(profile)
+            try {
+                displayMessage("success", 'You are now registered! <a href="/profile/login/" class="text-primary fw-bold">Log in</a> ', "#message");
+			} catch (error) {
+				displayMessage("danger", error, "#message");
+			}
 
 
         })

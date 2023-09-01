a33aa7ae6a54de796f8122a91fc00b8d122d0e6b
Ninuskaninus <89078328+Ninuskaninus@users.noreply.github.com>
2023-09-01 10:09:14 +0200

    added profile

--- js/feed.js
+++ js/feed.js
@@ -2,30 +2,6 @@ import { users } from "./users.js";
 
 //Containers
 const feedContainer = document.getElementById("feed-container");
-const headerContainer = document.getElementById("header-container");
-
-//Header
-const profilePicHeader = document.getElementById("profile-pic-header");
-profilePicHeader.src = users[0].profilePicture;
-
-//Heading and profile
-const profilePic = document.getElementById("profile-pic");
-profilePic.src = users[0].profilePicture;
-
-const profileName = document.getElementById("full-name");
-profileName.innerText = users[0].name;
-
-const profileUsername = document.getElementById("username");
-profileUsername.innerText = users[0].username;
-
-const nameContainer = document.getElementById("name-container");
-if (window.innerWidth < 577) {
-  nameContainer.classList.remove("mt-5");
-} else {
-  nameContainer.classList.add("mt-5");
-}
-
-console.log(nameContainer);
 
 //Upload
 const uploadContainer = document.createElement("div");

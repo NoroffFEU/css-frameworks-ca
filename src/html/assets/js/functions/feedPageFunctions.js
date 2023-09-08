6b0bf758008f029aaf2187c9106a7df454dd8d52
Kjell-Arne Neshagen <kjell-arne@neshagen.no>
2023-09-08 13:56:04 +0200

    Added feed placeholder to feed and profile page. Where profile page only shows feed from userId 1

---
+++ src/html/assets/js/functions/feedPageFunctions.js
@@ -0,0 +1,20 @@
+import { feedRow } from "../modules/selectedElements.js";
+import { Feed } from "../placeholders/feed.js";
+
+const listFeed = () => {
+  let feedList = "";
+  Feed.map(({title, body}) => {
+    feedList += `
+      <div class="card mb-5">
+      <div class="card-body">
+        <h4 class="card-title">${title}</h4>
+        <p class="card-text">${body}</p>
+        <a href="#" class="btn btn-primary" disabled>Read more..</a>
+      </div>
+    </div>
+    `;
+  });
+  feedRow.innerHTML = feedList;
+}
+
+listFeed();
--- src/html/assets/js/modules/selectedElements.js
+++ src/html/assets/js/modules/selectedElements.js
@@ -1,5 +1,6 @@
 export const userNameInput = document.querySelector("#username");
 export const userPasswdInput = document.querySelector("#passwd");
 export const submitBtn = document.querySelector(".submitBtn");
-
 export const errorElement = document.querySelector(".errorWrapper");
+export const feedRow = document.querySelector(".feed-row");
+export const profileFeedRow = document.querySelector(".profile-feed-row");

8448db048f4865132337a3d1578c70c2a50e5123
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-09-09 07:49:55 +0200

    fix updateProfile function

--- src/js/jwt.ts
+++ src/js/jwt.ts
@@ -1,88 +1,103 @@
 import { endpoints } from "./endpoints.js";
 
+type registerProp = "email" | "userName" | "password" | "repeatedPassword";
 
-type registerProp = "email"|"userName"|"password"|"repeatedPassword"
-
-interface registerInputValues{
-    email:string;
-    userName:string;
-    password:string;
-    repeatedPassword:string;
+interface registerInputValues {
+  email: string;
+  userName: string;
+  password: string;
+  repeatedPassword: string;
 }
 
-const registerObject:registerInputValues = {
-    email:"email",
-    userName:"username",
-    password:"password",
-    repeatedPassword:"password"
+const registerObject: registerInputValues = {
+  email: "email",
+  userName: "username",
+  password: "password",
+  repeatedPassword: "password",
+};
 
+const loginObject: { email: string; password: string } = {
+  email: "",
+  password: "",
+};
 
+const inputPassword = document.querySelector(
+  "#password--register"
+) as HTMLInputElement;
+const inputEmail = document.querySelector(
+  "#email--register"
+) as HTMLInputElement;
+const inputRpassword = document.querySelector(
+  "#password--register--repeat"
+) as HTMLInputElement;
+const inputUserName = document.querySelector(
+  "#username--register"
+) as HTMLInputElement;
+const loginUserName = document.querySelector(
+  "#username--login"
+) as HTMLInputElement;
+const loginPassword = document.querySelector(
+  "#password--login"
+) as HTMLInputElement;
+const buttonRegister = document.querySelector("#button--register");
+const buttonLogin = document.querySelector("#button--login");
+
+loginPassword.addEventListener("input", () => {
+  loginObject.password = loginPassword.value;
+  console.log(loginObject);
+});
+loginUserName.addEventListener("input", () => {
+  loginObject.email = loginUserName.value;
+  console.log(loginObject);
+});
+
+function collectInput(input: HTMLInputElement, inputName: registerProp) {
+  input.addEventListener("input", () => {
+    if (input.value) {
+      registerObject[inputName] = input.value;
+      console.log({ registerObject });
+    }
+    return;
+  });
 }
 
-const loginObject:{email:string,password:string}={email:"",password:""}
+collectInput(inputEmail, "email");
+collectInput(inputPassword, "password");
+collectInput(inputUserName, "userName");
+collectInput(inputRpassword, "repeatedPassword");
 
-
-
-const inputPassword = document.querySelector("#password--register") as HTMLInputElement
-const inputEmail = document.querySelector("#email--register") as HTMLInputElement
-const inputRpassword = document.querySelector("#password--register--repeat") as HTMLInputElement
-const inputUserName = document.querySelector("#username--register") as HTMLInputElement
-const loginUserName = document.querySelector("#username--login") as HTMLInputElement
-const loginPassword = document.querySelector("#password--login") as HTMLInputElement
-const buttonRegister = document.querySelector("#button--register")
-const buttonLogin = document.querySelector("#button--login")
-
-loginPassword.addEventListener("input",()=>{
-    loginObject.password=loginPassword.value
-    console.log(loginObject)
-})
-loginUserName.addEventListener("input",()=>{
-    loginObject.email=loginUserName.value
-    console.log(loginObject)
-
-})
-
-function collectInput(input:HTMLInputElement,inputName:registerProp){
-    input.addEventListener("input",()=>{
-        if(input.value){
-        registerObject[inputName] = input.value
-        console.log({registerObject})
- }return })
+async function registerAccount({
+  email,
+  userName,
+  password,
+}: registerInputValues) {
+  const response = await fetch(`${endpoints.baseUrl + endpoints.register}`, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ email, name: userName, password }),
+  });
+  const data = await response.json();
 }
 
-collectInput(inputEmail,"email")
-collectInput(inputPassword,"password")
-collectInput(inputUserName,"userName")
-collectInput(inputRpassword,"repeatedPassword")
+buttonRegister?.addEventListener("click", () => {
+  registerAccount(registerObject);
+});
+buttonLogin?.addEventListener("click", () => {
+  login(loginObject);
+});
 
-
-
-async function registerAccount({email,userName,password}:registerInputValues){
-    const response = await fetch(`${endpoints.baseUrl+endpoints.register}`
-    ,{method:"POST",headers:{"Content-type":"application/json"}
-    ,body:JSON.stringify({email,name:userName,password})}
-    )
-    const data = await response.json()
+async function login({ email, password }: typeof loginObject) {
+  const response = await fetch(`${endpoints.baseUrl + endpoints.login}`, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+  const data = await response.json();
+  console.log(data);
+  localStorage.setItem("token", JSON.stringify(data.accessToken));
+  changePage(data.name);
 }
 
-buttonRegister?.addEventListener("click",()=>{
-registerAccount(registerObject)
-})
-buttonLogin?.addEventListener("click",()=>{
-login(loginObject)
-})
-
-
-async function login({email,password}:typeof loginObject) {
-const response = await fetch(`${endpoints.baseUrl+endpoints.login}`,{method:"POST",headers:{"Content-type":"application/json"},body:JSON.stringify({email,password})})    
-const data = await response.json()
-console.log(data)
-localStorage.setItem("token",JSON.stringify(data.accessToken))
-changePage(data.name)
-}
-
-
-function changePage(name:string
-){
-    window.location.href=`/src/profile/index.html?user=${name}`
+function changePage(name: string) {
+  window.location.href = `/src/profile/index.html?user=${name}`;
 }

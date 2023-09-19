39592b87ce92265cd8304e31ab310da09235f5b8
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-09-19 13:25:19 +0200

    Create register.js

---
+++ js/register.js
@@ -0,0 +1,21 @@
+const API_BASE_URL = "https://api.noroff.dev/api/v1/social/auth/register";
+
+async function registerUser(url, data) {
+  try {
+    const postData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, postData);
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+    return json;
+  } catch (error) {
+    console.log(error);
+  }
+}

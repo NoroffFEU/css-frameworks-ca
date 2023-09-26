1cccea1d46f5a5e91084050e73aa96906ecebcdc
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-09-26 10:44:40 +0200

    Started on profile changes
    started on the profile Get request.
    Need to filter it down to the person logging in somehow.

---
+++ js/profileInfo.js
@@ -0,0 +1,25 @@
+const API_BASE_URL = "https://api.noroff.dev/api/v1/";
+
+async function createProfile(url) {
+  try {
+    console.log(url);
+    const token = localStorage.getItem("accessToken");
+    console.log(token);
+    const fetchProfileInfo = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(url, fetchProfileInfo);
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+const profileInfo = `${API_BASE_URL}social/profiles`;
+
+createProfile(profileInfo);

02294c784e65ccff202c80f7b868f655a9c847c6
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-09-28 15:14:19 +0200

    updated profile page
    dynamically build profile info on page from the user API.
    
    Follow button not in use right now.

--- js/profileInfo.js
+++ js/profileInfo.js
@@ -17,11 +17,93 @@ async function createProfile(url) {
     const response = await fetch(url, fetchProfileInfo);
     console.log(response);
     const json = await response.json();
+
+    //Populating the profile
+    const profileBox = document.querySelector(".profileBox");
+
+    const profileContainer = document.createElement("div");
+    profileContainer.classList.add("bg-dark", "text-center", "rounded", "p-3");
+    profileContainer.id = createProfile.id;
+
+    const profileName = document.createElement("h1");
+    profileName.classList.add("text-white", "bolder", "display-1");
+    profileName.innerText = json.name;
+    profileContainer.append(profileName);
+
+    const profileAvatar = document.createElement("img");
+    profileAvatar.classList.add("img-fluid", "rounded-circle", "mb-3", "mt-2");
+
+    if (json.avatar && json.avatar.trim() !== "") {
+      profileAvatar.src = json.avatar;
+    } else {
+      profileAvatar.src = "/images/profile.jpg";
+    }
+    profileContainer.append(profileAvatar);
+
+    const ul = document.createElement("ul");
+    ul.classList.add("list-unstyled", "mt-2", "fs-5");
+
+    const li1 = document.createElement("li");
+    const a1 = document.createElement("a");
+    a1.href = "#";
+    a1.classList.add(
+      "text-decoration-none",
+      "text-white",
+      "pe-auto",
+      "link-danger"
+    );
+    a1.innerText = "Contact";
+    li1.appendChild(a1);
+
+    const li2 = document.createElement("li");
+    const a2 = document.createElement("a");
+    a2.href = "#";
+    a2.classList.add(
+      "mt-5",
+      "text-decoration-none",
+      "text-white",
+      "pe-auto",
+      "link-danger"
+    );
+    a2.innerText = "Edit Profile";
+    li2.appendChild(a2);
+
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+
+    profileContainer.appendChild(ul);
+
+    const numberOfFollowers = document.createElement("p");
+    numberOfFollowers.classList.add("text-white", "bolder", "mt-2", "fs-4");
+    numberOfFollowers.innerText = "Followers:   " + json._count.followers;
+    numberOfFollowers.id = createProfile.id;
+    profileContainer.append(numberOfFollowers);
+
+    const numberOfFollowing = document.createElement("p");
+    numberOfFollowing.classList.add("text-white", "bolder", "mt-2", "fs-4");
+    numberOfFollowing.innerText = "Following:   " + json._count.following;
+    numberOfFollowing.id = createProfile.id;
+    profileContainer.append(numberOfFollowing);
+
+    const followButton = document.createElement("button");
+    followButton.type = "button";
+    followButton.classList.add(
+      "btn",
+      "btn-outline-danger",
+      "text-white",
+      "btn-lg"
+    );
+    followButton.innerText = "Follow";
+    profileContainer.appendChild(followButton);
+
+    profileBox.append(profileContainer);
+
     console.log(json);
   } catch (error) {
     console.log(error);
   }
 }
+
 const profileInfo = `${API_BASE_URL}social/profiles/${userName}`;
 
 createProfile(profileInfo);

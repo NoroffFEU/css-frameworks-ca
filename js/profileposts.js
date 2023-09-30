f51099ebf668dadc9c806be0cadb053049cab950
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-09-30 13:12:40 +0200

    updated profile posts Js
    fetching Profile posts and info
    added Fontawesome link to all pages

--- js/profileposts.js
+++ js/profileposts.js
@@ -1,6 +1,4 @@
 const API_Profiles_URL = "https://api.noroff.dev/api/v1/social/profiles";
-// const userName = localStorage.getItem("userName");
-//https://api.noroff.dev/api/v1/social/posts?_author=true
 
 async function gatherUserPosts(url) {
   try {
@@ -23,10 +21,85 @@ async function gatherUserPosts(url) {
     profilePosts.innerHTML = "";
 
     json.forEach((post) => {
-      const titleElement = document.createElement("div");
-      titleElement.classList.add("text-center", "rounded", "p-3");
-      titleElement.textContent = post.title;
-      profilePosts.appendChild(titleElement);
+      // Create a main container for each post with border
+      const postContainer = document.createElement("div");
+      postContainer.classList.add(
+        "post-container",
+        "border",
+        "mb-1",
+        "p-3",
+        "d-flex",
+        "flex-column"
+      );
+
+      // Create a container for the post content (image, name, date)
+      const posterInfo = document.createElement("div");
+      posterInfo.classList.add(
+        "d-flex",
+        "justify-content-start",
+        "justify-content-md-between",
+        "justify-content-sm-evenly",
+        "align-items-center",
+        "mb-3"
+      );
+
+      // Create and style the postAvatar element
+      const postAvatar = document.createElement("img");
+      postAvatar.classList.add("img-fluid", "rounded-circle");
+      postAvatar.style.width = "60px";
+
+      if (json.avatar && json.avatar.trim() !== "") {
+        postAvatar.src = json.avatar;
+      } else {
+        postAvatar.src = "/images/profile.jpg";
+      }
+
+      const postAuthor = document.createElement("p");
+      postAuthor.classList.add("text-start", "fw-bold");
+      postAuthor.textContent = post.author.name;
+
+      const createdDate = new Date(post.created);
+      const formattedDate = createdDate.toLocaleDateString();
+      const postDate = document.createElement("p");
+      postDate.classList.add("fst-italic", "d-none", "d-sm-block");
+      postDate.textContent = `${formattedDate}`;
+
+      // Append the elements to the postContentContainer
+      posterInfo.appendChild(postAvatar);
+      posterInfo.appendChild(postAuthor);
+      posterInfo.appendChild(postDate);
+
+      const postTitle = document.createElement("p");
+      postTitle.classList.add("text-start", "fs-5", "mt-1");
+      postTitle.textContent = post.title;
+
+      const postBody = document.createElement("p");
+      postBody.classList.add("fs-5", "fw-lighter");
+      postBody.textContent = post.body;
+
+      // Append the post content container to the main postContainer
+      postContainer.appendChild(posterInfo);
+
+      // Append the titleElement to the post content container
+      postContainer.appendChild(postTitle);
+      postContainer.appendChild(postBody);
+
+      if (post.media && post.media.trim() !== "") {
+        const postMedia = document.createElement("img");
+        postMedia.classList.add("img-fluid", "align-self-center");
+        postMedia.src = post.media;
+        postMedia.style.width = "100px";
+        postContainer.appendChild(postMedia);
+      } else {
+        // const postMedia = document.createElement("img");
+        // postMedia.classList.add("img-fluid", "align-self-center");
+        // postMedia.src = "/images/backgroundimage.jpg"; // Set the default image source
+        // postMedia.style.width = "200px";
+        // postContainer.appendChild(postMedia);
+      }
+
+      // Append the main postContainer to profilePosts
+      profilePosts.appendChild(postContainer);
     });
 
     console.log(json);
@@ -34,7 +107,7 @@ async function gatherUserPosts(url) {
     console.log(error);
   }
 }
-
-const userPosts = `${API_Profiles_URL}/${userName}/posts?_author=true`;
+//added a limit of 10 posts just so the page did not get flooded
+const userPosts = `${API_Profiles_URL}/${userName}/posts?_author=true&_sort=created&_limit=10`;
 
 gatherUserPosts(userPosts);

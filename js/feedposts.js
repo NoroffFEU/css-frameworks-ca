a684139e4de2e38892d7375dce5c7286595980bd
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-09-30 16:56:47 +0200

    updated the fetching all posts js
    building html from the fetch and styling

---
+++ js/feedposts.js
@@ -0,0 +1,103 @@
+const API_FEED_URL = "https://api.noroff.dev/api/v1/";
+
+async function fetchAllUserPosts(url) {
+  try {
+    console.log(url);
+    const token = localStorage.getItem("accessToken");
+    const fetchAllUserPostsOptions = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(url, fetchAllUserPostsOptions);
+
+    const json = await response.json();
+
+    const postWallContainer = document.querySelector(".postsWall");
+
+    postWallContainer.innerHTML = "";
+
+    json.forEach((post) => {
+      // Check if post.title or post.body exists and are not empty
+      if (!post.title || !post.body) {
+        return;
+      }
+      const postContainer = document.createElement("div");
+      postContainer.classList.add("col-md-12", "mb-3");
+
+      const postCard = document.createElement("div");
+      postCard.classList.add("card", "border", "rounded");
+
+      const postCardBody = document.createElement("div");
+      postCardBody.classList.add(
+        "card-body",
+        "d-flex",
+        "justify-content-start",
+        "justify-content-md-between",
+        "justify-content-sm-evenly",
+        "align-items-center",
+        "mb-3"
+      );
+
+      const postAvatar = document.createElement("img");
+      postAvatar.classList.add("img-fluid", "rounded-circle");
+      postAvatar.style.width = "60px";
+
+      // Assuming post.author has an avatar property
+      if (
+        post.author &&
+        post.author.avatar &&
+        post.author.avatar.trim() !== ""
+      ) {
+        postAvatar.src = post.author.avatar;
+      } else {
+        postAvatar.src = "/images/profile.jpg";
+      }
+
+      const postAuthor = document.createElement("p");
+      postAuthor.classList.add("card-text", "fw-bold");
+      postAuthor.textContent = post.author.name;
+
+      const createdDate = new Date(post.created);
+      const formattedDate = createdDate.toLocaleDateString();
+      const postDate = document.createElement("p");
+      postDate.classList.add("card-text", "fst-italic", "d-none", "d-sm-block");
+      postDate.textContent = formattedDate;
+
+      postCardBody.appendChild(postAvatar);
+      postCardBody.appendChild(postAuthor);
+      postCardBody.appendChild(postDate);
+
+      // Append the postCardBody to the postCard
+      postCard.appendChild(postCardBody);
+
+      const postTitle = document.createElement("p");
+      postTitle.classList.add("fw-bolder", "fs-3", "m-2");
+      postTitle.textContent = post.title;
+
+      const postBody = document.createElement("p");
+      postBody.classList.add("fs-4", "m-2");
+      postBody.textContent = post.body;
+
+      postCard.appendChild(postTitle);
+      postCard.appendChild(postBody);
+
+      // Append the postCard to the postContainer
+      postContainer.appendChild(postCard);
+
+      // Append each postContainer to the postsWall
+      const postWallContainer = document.querySelector(".postsWall");
+      postWallContainer.appendChild(postContainer);
+    });
+
+    console.log(json);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+const allPosts = `${API_FEED_URL}social/posts?_author=true`;
+
+fetchAllUserPosts(allPosts);

047396f5fb349c66012ffaa1735adbc49c347075
EliseAur <94222653+EliseAur@users.noreply.github.com>
2023-10-06 15:51:50 +0200

    Added registration form

--- src/js/bsFormValidation.js
+++ src/js/bsFormValidation.js
@@ -1,4 +1,5 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
+
 (() => {
     "use strict";
 
@@ -21,3 +22,5 @@
         );
     });
 })();
+
+console.log("hello");
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import * as constants from "./api/constants";
+
+console.log(constants.API_SOCIAL_URL);
+
+console.log("heipådeg");

459790b7497bfa158efd1c062651a471637eab95
Jarle302 <107812980+Jarle302@users.noreply.github.com>
2023-10-13 16:04:48 +0200

    add reaction and comment count

--- src/js/renderPost.js
+++ src/js/renderPost.js
@@ -1,5 +1,5 @@
 import createElementFactory from "./createElementFactory.js";
-export default function renderPosts(domEl, { id, title, body, tags, media, created, updated, _count, author, comments }) {
+export default function renderPosts(domEl, { id, title, body, tags, media, created, updated, _count, author, comments, reactions = [""], }) {
     const currentUser = JSON.parse(localStorage.getItem("currentUser"));
     const container = createElementFactory("div", "", domEl, { "data-observed": "", id: `div${id}` }, "card", "mb-3", "bg-white", "p-2", "w-percentage--95");
     const row = createElementFactory("div", "", container, {}, "row", "mb-4");
@@ -17,16 +17,26 @@ export default function renderPosts(domEl, { id, title, body, tags, media, creat
         id: `title${id}`,
     });
     const paragraph = createElementFactory("p", body, divCol8, { id: `body${id}` }, "card-text", "text-black");
+    const reactionContainer = createElementFactory("div", "", divCol8, {});
     const deleteButton = createElementFactory("button", "delete", divCol8, { id: `button--${id}`, "data-delete-id": id }, currentUser !== author.name && "hide", "btn", "btn-success");
     const updateButton = createElementFactory("button", "update", divCol8, { "data-update-id": id, id: `button--edit--${id}` }, currentUser !== author.name && "hide", "btn", "btn-outline-primary");
     const picturePost = media
         ? createElementFactory("img", "", divCol8, { src: media }, "w-100", "h-50")
         : "";
-    tags === null || tags === void 0 ? void 0 : tags.forEach((tag) => createElementFactory("span", tag, anker, {}, "badge", "text-bg-primary", "m-1", `tag${id}`));
+    const tagContainer = createElementFactory("div", "", anker, {});
+    tags === null || tags === void 0 ? void 0 : tags.forEach((tag) => createElementFactory("span", tag, tagContainer, {}, "badge", "text-bg-primary", "m-1", `tag${id}`));
+    if (reactions[0]) {
+        reactions.forEach((emoji) => createElementFactory("span", emoji.symbol + `(${emoji.count})`, reactionContainer, {}, "badge", "text-bg-primary", "m-1"));
+    }
     const reactButton = createElementFactory("button", "React", anker, {
         type: "button",
         "data-id": id,
     }, "btn", "btn-outline-primary");
+    if (_count) {
+        const countContainer = createElementFactory("div", "", anker, {});
+        const commentCount = createElementFactory("span", `💬comments(${_count.comments})`, countContainer, {}, "badge", "text-bg-primary");
+        const reactionCount = createElementFactory("span", `😊😡reactions(${_count.reactions})`, countContainer, {}, "badge", "text-bg-primary");
+    }
     const commentContainer = createElementFactory("div", "", container, {}, "container");
     const commentHeader = createElementFactory("h3", "Comments", commentContainer, {});
     const inputGroupContainer = createElementFactory("div", "", commentContainer, {}, "input-group", "mb-4");

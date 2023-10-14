cce7063963b99af16f1149b1de0fe41d183db82a
Christelmarita <christelmarita.oe@gmail.com>
2023-10-15 01:06:59 +0200

    create post function added

--- scripts/common/constant.mjs
+++ scripts/common/constant.mjs
@@ -2,3 +2,4 @@ export const API_BASE_URL = 'https://api.noroff.dev/api/v1';
 export const API_REGISTER_URL = `${API_BASE_URL}/social/auth/register`;
 export const API_LOGIN_URL = `${API_BASE_URL}/social/auth/login`;
 export const API_ALL_POSTS = `${API_BASE_URL}/social/posts?_author=true`;
+export const API_CREATE_POST = `${API_BASE_URL}/social/posts`;

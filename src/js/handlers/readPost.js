1c22950180f5e0f532eb81f81b4f772a4acb9029
EliseAur <94222653+EliseAur@users.noreply.github.com>
2023-10-25 17:47:21 +0200

    Deleted code

---
+++ src/js/handlers/readPost.js
@@ -0,0 +1,7 @@
+// document.addEventListener("click", function (event) {
+//     if (event.target.matches("[data-post-id]")) {
+//         const postId = event.target.getAttribute("data-post-id");
+//         // Redirect to the post detail page with the selected Post ID
+//         window.location.href = `post-detail.html?id=${postId}`;
+//     }
+// });

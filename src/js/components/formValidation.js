fe4961ea51d142b5d1cfef442f6e1f655a934dba
Vegard M <vm932648@gmail.com>
2023-10-29 13:45:42 +0100

    improving form validation and creating a signup  form
    The form validation wasn't quite right  yet, but I've fixed this now.
    
    I wanted the formCheck function  to be generic and reusable, so things like regEx is handled in HTML via the pattern attribute.
    
    Other than that, I've added a sign up form and made very minor chagnes in scss

--- src/js/components/formValidation.js
+++ src/js/components/formValidation.js
@@ -1,21 +1,38 @@
 const formInputs = document.querySelector("fieldset").elements;
 const btn = document.querySelector(".btn-primary");
 
+/**
+ * 
+ * @param {arrayOfBooleans} arr 
+ * @returns false, unless all Booleans are true
+ */
+const trueChecker = arr => arr.every(Boolean);
+
 
 /**
- * A simple form validation. Checks all inputs, and if all inputs are validated in the HTML, you're good! 
+ * A simple form validation. Checks all inputs the validity of all inputs (validated directly in HTML) and creates an array of
+ * booleans. Afterwards, the trueChecker function checks if all the booleans are true, and then I extract a variable from  this. 
+ * If all the form inputs are validated in HTML, you're good to go!
+ * 
  * There is a "pattern" attribute in the email input, which handles regex for me. I could have added this via JS, 
- * but hardcoding the regex in HTML did the same job
+ * but hardcoding the regex in HTML did the same job and keeps the code generic and reusable. 
  */
 function formCheck() {
+    const trueCheckerArr = [];
 
     for (let i = 0; i < formInputs.length; i++) {
-        if (formInputs[i].validity.valid) {
-            btn.disabled = false;
-            
-        }else{
-            btn.disabled = true;
-        }
+
+        const inputsValidity = formInputs[i].validity.valid;
+        trueCheckerArr.push(inputsValidity);
+        
+    }
+    
+    const checker =  trueChecker(trueCheckerArr);
+
+    if (!checker) {
+        btn.disabled = true;
+    } else  {
+        btn.disabled = false;
     }
 }
 

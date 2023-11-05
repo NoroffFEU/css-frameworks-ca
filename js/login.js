9f9e5716b0a4444b25f495da70839eb67897f3fa
KristianNilsenAug22FT <114563561+KristianNilsenAug22FT@users.noreply.github.com>
2023-11-05 22:34:35 +0100

    adde error handling in login.js

--- js/login.js
+++ js/login.js
@@ -14,35 +14,45 @@ document.addEventListener('DOMContentLoaded', function () {
                 body: JSON.stringify(data),
             };
             const response = await fetch(url, postData);
-            console.log(response);
-            const json = await response.json();
-
-            if (json.accessToken) {
-                // Store the access token in local storage
-                localStorage.setItem('accessToken', json.accessToken);
-                if (json.name) {
-                    // Store the user's name in local storage
-                    localStorage.setItem('userName', json.name);
+            
+            if (response.status === 200) {
+                const json = await response.json();
+    
+                if (json.accessToken) {
+                    // Store the access token in local storage
+                    localStorage.setItem('accessToken', json.accessToken);
+                    if (json.name) {
+                        // Store the user's name in local storage
+                        localStorage.setItem('userName', json.name);
+                    }
+                    console.log('Login successful');
+                    // Redirect to profile page after successful login
+                    window.location.href = 'profile/index.html';
+                } else {
+                    // Handle the case when the response is not as expected
+                    handleInvalidResponse('Login failed. Please check your credentials.');
                 }
-                console.log('Login successful');
-                // Redirect to profile page after successful login
-                window.location.href = 'profile/index.html';
+            } else if (response.status === 401) {
+                // Handle unauthorized (e.g., invalid credentials)
+                handleInvalidResponse('Invalid credentials. Please try again.');
             } else {
-                // Display the error message when login fails
-                const errorMessage = document.getElementById('login-error-message');
-                errorMessage.textContent = 'Login failed. Please check your credentials.';
-                errorMessage.classList.remove('d-none');
-                console.error('Login failed:', json.message);
+                // Handle other HTTP status codes
+                handleInvalidResponse('An error occurred. Please try again later.');
             }
         } catch (error) {
             // Handle network or other errors
-            const errorMessage = document.getElementById('login-error-message');
-            errorMessage.textContent = 'An error occurred. Please try again later.';
-            errorMessage.classList.remove('d-none');
+            handleInvalidResponse('An error occurred. Please try again later.');
             console.error('Error:', error);
         }
     }
-
+    
+    function handleInvalidResponse(errorMessage) {
+        // Display the error message when login fails or response is invalid
+        const errorMessageElement = document.getElementById('login-error-message');
+        errorMessageElement.textContent = errorMessage;
+        errorMessageElement.classList.remove('d-none');
+    }
+    
     const loginForm = document.getElementById('login-form');
     const emailField = document.getElementById('exampleInputEmail1');
     const passwordField = document.getElementById('exampleInputPassword1');

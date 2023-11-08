f4713d35f8d9fa8818756ff59ae661fa9f6775f0
Vegard M <vm932648@gmail.com>
2023-11-08 12:13:32 +0100

    Fucking around, not finding out
    Trying to get my search function up and running, but not getting there yet.
    
    I'm able to filter by title content, but only when it's an exact match. And that's not good enough. Will keep on keeping on

--- src/js/components/getActions.mjs
+++ src/js/components/getActions.mjs
@@ -7,7 +7,7 @@
  * This function takes the array of posts, and a DOM element, then produces
  * HTML based on the array in  our given HTML-element. 
  * 
- * Posts without content are skipped because I think they look ugly and don't 
+ * Posts without title or content are skipped because I think they look ugly and don't 
  * provide anything to the page.
  */
 export function printFeed(postsArray, domElement) {
@@ -22,7 +22,7 @@ export function printFeed(postsArray, domElement) {
         const slicedPostDate = postedDate.slice(0, postedDate.length - 14).split(".").reverse().join(".");
         const slicedEditDate = editDate.slice(0, postedDate.length - 14).split(".").reverse().join(".");
 
-        if (!post.body) {
+        if (!post.body || !post.title) {
             continue;
         }
         if (post.author.name === myUserName) {
@@ -105,13 +105,32 @@ export function sortArray(postsArray, domElement, sortBy){
 
 
 export function searchArray(postsArray, domElement, searchQuery) {
+    let filteredArray;
     console.log(searchQuery);
+    const searchWord = searchQuery.searchText.toLowerCase();
     const searchIn = searchQuery.searchKeys;
     console.log(searchIn);
-    if (searchIn === "title") {
-        const filteredArray = postsArray.filter((searchIn) => postsArray.toLowerCase().includes(searchQuery.toLowerCase()));
+    if (!searchIn){
+
+    } else if (searchIn === "title") {
+        // filteredArray = postsArray.filter(post => {
+        //     return post.title.toLowerCase() === searchWord;
+        // })
+        // console.log(filteredArray);
+         // -------------- Think I'm onto something here, 
+        // but I got to search the whole array. Will try a forloop -----------
+
+        // for (let i = 0; i < postsArray.length; i++) {
+        //     const element = postsArray[i];
+        //     if (element.title.toLowerCase().includes(searchWord)) {
+        //         return element;
+        //     }
+        // }
+        console.log(filteredArray);
+        } else if ( searchIn === "body"){
+
+        }
         // Working on this, but taking a break. I might set up another switch
         // where I either filter or don't filter based on what's in the searchkeys attribute of my query
     }
     
-}

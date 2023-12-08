b2c978bd03e000dbee1b73dedfeac7af95b7ad63
EF-Pami <pamijoelpami@gmail.com>
2023-12-08 23:23:20 +0100

    update on create.mjs

--- src/js/posts/create.mjs
+++ src/js/posts/create.mjs
@@ -1,5 +1,6 @@
 
 import { API_BASE_URL } from "../routes.mjs";
+import { fetchwithToken } from "./index.mjs";
 
 export async function createNewPost (postTitle, postContent, accessToken) {
     try {
@@ -32,3 +33,31 @@ export async function createNewPost (postTitle, postContent, accessToken) {
         return null;
     }
 }
+
+fetchwithToken(API_BASE_URL + '/posts');
+
+const createpost = document.getElementById('createpost');
+
+createpost.addEventListener('submit', async function (event) {
+    event.preventDefault();
+
+    const postTitle = document.getElementById('postTitle').value;
+    const postContent = document.getElementById('postContent').value;
+
+    const accessToken = localStorage.getItem('accessToken');
+
+    const newPost = await createNewPost(postTitle, postContent, accessToken);
+
+    if (newPost) {
+        const postscontainer = document.getElementsByClassName('postscontainer');
+        const postElement = document.createElement('div');
+        postElement.innerHTML =
+            `<h3 class="content-font">${newPost.title}</h3>
+            <p class="content-font">${newPost.body}</p>`;
+
+            postscontainer.appendChild(postElement);
+
+            document.getElementById('postTitle').value = '';
+            document.getElementById('postContent').value = '';
+    }
+});

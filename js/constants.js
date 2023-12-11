666b02172bce8a1c533865de2eab0b6c95bd45b5
NiklasFerling <niklas.ferling@gmail.com>
2023-12-11 16:03:39 +0100

    Changes constant name

--- js/constants.js
+++ js/constants.js
@@ -3,11 +3,10 @@ export const API_BASE_URL = "https://api.noroff.dev/api/v1/social";
 export const registerUrl = `${API_BASE_URL}/auth/register`;
 export const loginUrl = `${API_BASE_URL}/auth/login`;
 export const getPostsUrl = `${API_BASE_URL}/posts?_author=true&_comments=true&_reactions=true`;
-export const postUrl = `${API_BASE_URL}/posts`;
+export const createPostUrl = `${API_BASE_URL}/posts`;
 
 // HTML Elements
 export const loader = document.querySelector(".loader");
-export const profile = document.querySelector(".profile");
 export const reactions = document.querySelector(".reactions");
 export const comments = document.querySelector(".comments");
 

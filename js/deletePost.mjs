feec4c49e0a73a99c5dcfc2bea43cbea4a40d4f3
CharlotteValset <119309299+CharlotteValset@users.noreply.github.com>
2023-12-21 15:02:04 +0100

    Added delete post function with modal popup

--- js/deletePost.mjs
+++ js/deletePost.mjs
@@ -1,142 +1,56 @@
 import { apiBaseUrl, allPostsApi } from "./variables.mjs";
 import { fetchWithToken, token } from "./accessToken.mjs";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const queryString = document.location.search;
-  const params = new URLSearchParams(queryString);
-  const postId = params.get("id");
-
-  console.log("postId", postId);
-
-  const removePostLink = document.querySelector("#delete-post");
-  console.log("removePostLink", removePostLink);
-  const deleteConfirmationModal = document.querySelector("#deleteConfirmationModal");
-  console.log("deleteConfirmationModal", deleteConfirmationModal);
-  const confirmDeleteButton = document.querySelector("#confirmDelete");
-  console.log("deleteConfirmationModal", deleteConfirmationModal);
-  const cancelDeleteButton = document.querySelector("#cancelDelete");
-  console.log("cancelDeleteButton", cancelDeleteButton);
-
-  removePostLink.addEventListener("click", (event) => {
-    event.preventDefault();
-    console.log("Delete button clicked");
-    // Show the modal when the delete button is clicked
-    deleteConfirmationModal.style.display = "block";
-  });
-
-  // Event listener for confirming deletion
-  confirmDeleteButton.addEventListener("click", async () => {
+/**
+ * Deletes a post with the specified ID after confirmation
+ *
+ * @param {number} id - The ID of the post to be deleted
+ * @returns {Promise<void>} - A Promise that resolves when the deletion is successful
+ */
+async function deletePost(id) {
     try {
       // Perform the deletion when confirmed
-      const response = await fetchWithToken(`${apiBaseUrl}${allPostsApi}/${postId}`, {
+      const response = await fetchWithToken(`${apiBaseUrl}${allPostsApi}/${id}`, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
       });
-
-      if (response.ok) {
-        // Redirect to the profile page after successful deletion
-        window.location.href = "/profile/";
+      if (response.error) {
+          // Handle unsuccessful deletion, e.g., show an error message
+          throw new Error("Error deleting post:", response.statusText);
       } else {
-        // Handle unsuccessful deletion, e.g., show an error message
-        console.error("Error deleting post:", response.statusText);
+        alert ("Post is deleted!");
+          // Redirect to the profile page after successful deletion
+          window.location.href = "/profile/";
       }
     } catch (error) {
-      console.error("Error deleting post:", error);
+      throw new Error("Error deleting post:", error);
     }
-  });
-
-  // Event listener for canceling deletion
-  cancelDeleteButton.addEventListener("click", () => {
-    // Hide the modal when canceled
-    deleteConfirmationModal.style.display = "none";
-  });
-});
-
-/* 
-// Query string parameter
-const queryString = document.location.search;
-const params = new URLSearchParams(queryString);
-const postId = params.get("id");
-
-async function removePost(event) {
-  event.preventDefault();
-
-  const title = event.target.querySelector("#editTitle");
-  const content = event.target.querySelector("#editBodyText")
-  const imageUrl = event.target.querySelector("#editImage");
-
-  if (!title || !content) {
-    alert("Please fill in all required fields");
-    return;
-  }
-
-  const editPostData = {
-    title: title.value,
-    body: content.value,
-    media: imageUrl.value,
   };
-  
-  try {
-    const response = await fetchWithToken(`${apiBaseUrl}${allPostsApi}/${postId}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(editPostData),
-    });
 
-      window.location.href = "/profile/";
-
-  } catch (error) {
-    console.error("Error updating post:", error);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const removePostForm = document.querySelector("#delete-post");
-  removePostForm.addEventListener("click", removePost);
-});
-
-
- */
-
-/* document.addEventListener("DOMContentLoaded", () => {
-  const removePostForm = document.querySelector("#delete-post");
-  const deleteConfirmationModal = document.querySelector("#deleteConfirmationModal");
-  const confirmDeleteButton = document.querySelector("#confirmDelete");
   const cancelDeleteButton = document.querySelector("#cancelDelete");
-
-  removePostForm.addEventListener("click", () => {
-    // Show the modal when the delete button is clicked
-    deleteConfirmationModal.style.display = "block";
-  });
-
-  // Event listener for confirming deletion
-  confirmDeleteButton.addEventListener("click", async () => {
-    try {
-      // Perform the deletion when confirmed
-      const response = await fetchWithToken(`${apiBaseUrl}${allPostsApi}/${postId}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      // Redirect to the profile page after successful deletion
-      window.location.href = "/profile/";
-    } catch (error) {
-      console.error("Error deleting post:", error);
-    }
-  });
+  const deleteConfirmationModal = document.querySelector("#deleteConfirmationModal");
 
   // Event listener for canceling deletion
   cancelDeleteButton.addEventListener("click", () => {
     // Hide the modal when canceled
     deleteConfirmationModal.style.display = "none";
   });
-}); */
+  
+
+  const confirmDeleteButton = document.querySelector("#confirmDelete");
+    // Event listener for canceling deletion
+    confirmDeleteButton.addEventListener("click", () => {
+
+      const postId = deleteConfirmationModal.getAttribute("data-post-id");
+
+      if (postId) {
+        deletePost(postId);
+      } else {
+        throw new Error("postId is not defined or has an incorrect value.");
+      }
+      // Hide the modal when canceled
+      deleteConfirmationModal.style.display = "none";
+    });

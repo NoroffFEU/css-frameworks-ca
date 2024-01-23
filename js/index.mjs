949d62f6d2caf9268dffcb7bdac3d111abdf720c
KariannNor <KarNor13066@stud.noroff.no>
2024-01-23 12:12:22 -0500

    Error and success message handling on register

--- js/index.mjs
+++ js/index.mjs
@@ -7,7 +7,7 @@ const path = location.pathname;
 
 if (path === '/index.html' || path === '/') {
   loginFormListener();
-} else if (path === '/create_account/index.html') {
+} else if (path === '/create-account/index.html') {
   registerFormListener();
 } else {
   console.error(`There seems to be issues with path: ${path}`);

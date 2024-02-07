79d7abb4a4b044449e8d6849334e18e37e3bf5f0
KariannNor <KarNor13066@stud.noroff.no>
2024-02-07 11:55:42 -0500

    Updated location.href

--- js/handler/login/index.mjs
+++ js/handler/login/index.mjs
@@ -2,31 +2,29 @@ import { login } from "../../api/auth/login.mjs";
 import * as storage from "../../handler/storage.mjs";
 import { displayMessage } from "../../ui/displayMessage.mjs";
 
-
 export function loginFormListener() {
   const form = document.querySelector("#loginForm");
 
   if (form) {
-  form.addEventListener("submit", dologin); 
+    form.addEventListener("submit", dologin);
   }
-}  
-
-async function dologin(event) {
-      event.preventDefault()
-        const form = event.target;
-        const formData = new FormData(form);
-        const profile = Object.fromEntries(formData.entries())
-
-        try {
-          const response = await login(profile);
-          const { accessToken, name } = response;
-
-          storage.save("token", accessToken);
-          storage.save("username", name);
-
-          location.href = "/feed/create";
-      } catch (error) {
-        displayMessage("#message", "danger", error.message);
-      }
 }
 
+async function dologin(event) {
+  event.preventDefault();
+  const form = event.target;
+  const formData = new FormData(form);
+  const profile = Object.fromEntries(formData.entries());
+
+  try {
+    const response = await login(profile);
+    const { accessToken, name } = response;
+
+    storage.save("token", accessToken);
+    storage.save("username", name);
+
+    location.href = "/feed/post/";
+  } catch (error) {
+    displayMessage("#message", "danger", error.message);
+  }
+}

4e51399447cb894257fc25cd1e54c30b87384881
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-02-15 14:04:09 +0100

    js added
    constants and register added

---
+++ js/api/api_constants.mjs
@@ -0,0 +1,21 @@
+export const API_HOST_URL = 'https://nf-api.onrender.com'
+export const API_BASE ='/api/v1';
+export const API_AUTH = '/auth';
+export const API_REGISTER ='/register'
+export const API_LOGIN ='/login';
+export const API_SOCIAL_BASE ='/social';
+export const API_KEY_URL ='/create-api-key';
+
+export const API_SOCIAL_URL = `${API_HOST_URL}${API_BASE}${API_SOCIAL_BASE}`;
+
+export function save (key, value){
+    localStorage.setItem (key, JSON.stringify(value));
+}
+
+export function load(key){
+    return JSON.parse(localStorage.getItem(key));
+
+}
+export function displayErrorMessage(message) {
+    alert(message);
+}

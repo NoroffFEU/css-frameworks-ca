ab3a860c99c45d9c953342461af2b7a03cbdbfbe
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-02-16 21:51:28 +0100

    js-base for posts created
    get, create, delete, update post js-base created

--- js/index.mjs
+++ js/index.mjs
@@ -1,5 +1,9 @@
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
+import { removePost } from "./api/posts/delete.mjs";;
+import { updatePost } from "./api/posts/update.mjs";
+
+import * as posts from "./api/posts/index.mjs";
 
 const path = location.pathname;
 
@@ -8,3 +12,20 @@ if (path === "/profile/login/index.html") {
 } else if (path === "/profile/register/") {
   setRegisterFormListener();
 }
+
+updatePost({
+    id: 10470,
+    title: "example Post UPDATED",
+    body: "example nr 2 UPDATED"
+})
+
+
+
+// posts.createPost();
+// posts.updatePost();
+// posts.removePost();
+// posts.getPost();
+// posts.getPosts().then(console.log);
+posts.removePost(10470)
+
+

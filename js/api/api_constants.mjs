ffc6c417817b2e757b343db87ed1d98c6d75840a
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-02-17 13:54:32 +0100

    constants edited

--- js/api/api_constants.mjs
+++ js/api/api_constants.mjs
@@ -8,13 +8,13 @@ export const API_KEY_URL = "/create-api-key";
 
 export const API_SOCIAL_URL = `${API_HOST_URL}${API_BASE}${API_SOCIAL_BASE}`;
 
-export function save(key, value) {
-  localStorage.setItem(key, JSON.stringify(value));
-}
+// export function save(key, value) {
+//   localStorage.setItem(key, JSON.stringify(value));
+// }
 
-export function load(key) {
-  return JSON.parse(localStorage.getItem(key));
-}
-export function displayErrorMessage(message) {
-  alert(message);
-}
+// export function load(key) {
+//   return JSON.parse(localStorage.getItem(key));
+// }
+// export function displayErrorMessage(message) {
+//   alert(message);
+// }

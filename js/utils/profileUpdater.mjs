e33cd2ed0fec24542d77aaa1f6a6e9b13931e360
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-02-19 12:40:23 +0100

    avatar and banner update
    added avatar and banner
    added default avatar and banner
    profileUpdater.mjs updated with banner and avatar

--- js/utils/profileUpdater.mjs
+++ js/utils/profileUpdater.mjs
@@ -13,5 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
         userProfile.name
       );
     });
+
+    // Replace "img" with the user's avatar image URL or default avatar if not provided
+    const elementsWithAvatar = document.querySelectorAll(".replace-avatar");
+    elementsWithAvatar.forEach((element) => {
+      const avatarImage = userProfile.avatar ? userProfile.avatar : "../../image/default-avatar.JPG";
+      element.src = avatarImage;
+    });
+
+    // Set banner image
+    const bannerImage = document.querySelector(".profile-banner");
+    if (bannerImage) {
+      if (userProfile.banner) {
+        bannerImage.style.backgroundImage = `url(${userProfile.banner})`;
+      } else {
+        // Use default banner image if userProfile.banner is null
+        bannerImage.style.backgroundImage = `url(../../image/default-banner.JPG)`;
+      }
+    }
   }
 });

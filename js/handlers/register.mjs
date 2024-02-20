053c840b84d5dd4e86b7997fed367e5397e51efe
IceGreenGalactic <125923103+IceGreenGalactic@users.noreply.github.com>
2024-02-20 22:49:01 +0100

    register update DOMContentLoaded
    DOMContentLoaded added to make the register work

--- js/handlers/register.mjs
+++ js/handlers/register.mjs
@@ -1,17 +1,33 @@
 import { register } from "../api/auth/register.mjs";
 
 
-export function setRegisterFormListener(){
+export function setRegisterFormListener() {
+  
   const form = document.querySelector("#registrationForm");
 
+
   form.addEventListener("submit", (event) => {
-    event.preventDefault(); 
-    const form = event.target;
+    event.preventDefault();
     const formData = new FormData(form);
     const profile = Object.fromEntries(formData.entries());
-  
 
-    // Send registration request to the API
-    register (profile)
+
+    // Send it to the API
+    register(profile)
+      .then(() => {
+     
+      })
+      .catch((error) => {
+        console.error("Error registering:", error);
+      });
+  
   });
 }
+
+// Call the function inside the DOMContentLoaded event listener
+document.addEventListener("DOMContentLoaded", function() {
+  console.log("Script loaded");
+
+  // Call the function to set up the listener
+  setRegisterFormListener();
+});

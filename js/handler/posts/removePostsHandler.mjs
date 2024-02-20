8e48a69f227dc69fbf170e21126da2f5f41613c1
KariannNor <KarNor13066@stud.noroff.no>
2024-02-20 15:56:01 -0500

    removePostHandler edited

--- js/handler/posts/removePostsHandler.mjs
+++ js/handler/posts/removePostsHandler.mjs
@@ -1,25 +1,28 @@
 import { removePost } from "../../api/posts/index.mjs";
 
 export async function removePostsHandler() {
-  const removePostBtn = document.querySelectorAll(`[data-action="delete"]`);
-  removePostBtn.forEach((button) => {
-    button.addEventListener("click", handleDeletePost);
+  document.addEventListener("DOMContentLoaded", () => {
+    const removePostBtns = document.querySelectorAll(`[data-action="delete"]`);
+    removePostBtns.forEach((button) => {
+      button.addEventListener("click", handleDeletePost);
+    });
   });
 }
 
 async function handleDeletePost(event) {
-  const id = event.target.dataset.id;
-  await removePost(id);
-  window.location.reload();
+  event.preventDefault();
 
-  const parentElement = event.target.parentElement;
+  const id = event.target.dataset.id;
+  const parentElement = event.target.closest("#card");
 
   const confirmRemovePost = confirm(
     "Are you sure you want to delete this post?"
   );
+
   if (confirmRemovePost) {
-    await deletePost(id);
+    await removePost(id);
     parentElement.remove();
     messageForUser("#modalDelete", "success", "Post deleted successfully");
+    window.location.reload();
   }
 }

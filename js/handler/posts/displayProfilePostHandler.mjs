2610c7127fc41cfb3da8cc0650b06e2e8e31bfea
KariannNor <KarNor13066@stud.noroff.no>
2024-02-20 08:02:47 -0500

    double call removed

--- js/handler/posts/displayProfilePostHandler.mjs
+++ js/handler/posts/displayProfilePostHandler.mjs
@@ -10,4 +10,4 @@ export async function displayProfilePostHandler() {
   renderProfilePosts(userPosts, container);
 }
 
-displayProfilePostHandler();
+// displayProfilePostHandler();
--- js/templates/renderProfilePost.mjs
+++ js/templates/renderProfilePost.mjs
@@ -88,3 +88,4 @@ function displayPost(post) {
 
   return postSection;
 }
+
